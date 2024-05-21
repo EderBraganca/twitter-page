25b8a793818da90f432d8dd7c1056288d3740b88
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-05-21 12:18:12 -0300

    St;yle update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';  // Alteração na importação
+import { createRoot } from 'react-dom/client'; 
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
